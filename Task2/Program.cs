76aac2bc607c4f0a39cd53baca517854c96e1e1c
Artem Golubev <lesovba@gmail.com>
2023-05-24 20:40:41 +0500

    Completed Task #2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,3 +4,32 @@
 [3, 7, 23, 12] -> 19
 
 [-4, -6, 89, 6]-> 0*/
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(1, 1000);
+        Console.Write($" {array[i]} ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void FindOddIndexesElemSum(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+           sum +=arr[i];  
+    }
+    Console.WriteLine($"Сумма элементов массива с нечётными индексами равна {sum}");
+}
+
+Console.WriteLine();
+FindOddIndexesElemSum(GetArray(10)); 
+Console.WriteLine(); 
